555061233e3770d9850e986c9245560fccda1e39
Hoang My <hoangmyit@outlook.com>
2019-04-04 23:56:54 +0700

    update in house

--- 2. Source Code/DataAccess/UserDAO.cs
+++ 2. Source Code/DataAccess/UserDAO.cs
@@ -1,9 +1,15 @@
 ﻿using DataAccess.Interface;
+using Entity;
 using System;
 
 namespace DataAccess
 {
     public class UserDAO : IUserDAO
     {
+        private readonly JWTDemoDbContext _jwtDemoDbContext;
+        public UserDAO()
+        {
+           
+        }
     }
 }
